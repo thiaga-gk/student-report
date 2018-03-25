b8811e61183c6ded209c5131dc7090d40a32d6f2
Thiagaraja Gurusamy <thiagaraja.gurusamy@gmail.com>
2018-03-24 23:41:15 -0400

    added project structure

--- src/main/java/com/student/report/StudentReportApplication.java
+++ src/main/java/com/student/report/StudentReportApplication.java
@@ -1,4 +1,4 @@
-package com.student.report.studentreport;
+package com.student.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
